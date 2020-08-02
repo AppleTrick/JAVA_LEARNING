9be2e6c7e5899c368426bb4b0535117cef4f37be
AppleTrick <arrans9591@gmail.com>
2020-08-02 19:58:42 +0900

    오버로딩

---
+++ src/overloadingPractice/Overloading.java
@@ -0,0 +1,33 @@
+package overloadingPractice;
+
+public class Overloading {
+    public static void main(String[] args) {
+        Person man = new Person(951025, 1870008467);
+        Person woman = new Person(951025);
+        man.showInfo();
+        woman.showInfo();
+    }
+}
+
+class Person {
+    private int perId;
+    private int milId;
+
+    public Person(int pId, int mId){
+        perId = pId;
+        milId = mId;
+    }
+    public Person(int pId){
+        perId = pId;
+        milId = 0;
+    }
+    public void showInfo() {
+        System.out.println("민번 : " + perId);
+        if (milId != 0) {
+            System.out.println("군번 " + milId + '\n');
+        } else {
+            System.out.println("군과 관게 없음 \n");
+        }
+    }
+
+}
