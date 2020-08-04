9cac421d79ea99b7ef6c9040e465c2c16681b30d
AppleTrick <arrans9591@gmail.com>
2020-08-04 22:42:24 +0900

    백준 테스트 10869 10439

---
+++ src/CodingTest_BAEKJOON/BASIC/Basic005.java
@@ -0,0 +1,19 @@
+package CodingTest_BAEKJOON.BASIC;
+
+import java.util.Scanner;
+
+public class Basic005 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
+
+//백준 10869
---
+++ src/CodingTest_BAEKJOON/BASIC/Basic006.java
@@ -0,0 +1,18 @@
+package CodingTest_BAEKJOON.BASIC;
+//백준 10430
+
+import java.util.Scanner;
+
+public class Basic006 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println((a+b)%c);
+        System.out.println(((a%c) + (b%c))%c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c) * (b%c))%c);
+    }
+}
