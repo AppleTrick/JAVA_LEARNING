749127903d14581d79e2cfa427fb9f81332548c4
AppleTrick <arrans9591@gmail.com>
2020-08-10 22:21:25 +0900

    예외처리 공부 1

---
+++ src/ExceptionPractice/Practice1/ExceptionCase1.java
@@ -0,0 +1,21 @@
+package ExceptionPractice.Practice1;
+
+import java.util.Scanner;
+
+public class ExceptionCase1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.println("a값을 입력하세요");
+            int num1 = sc.nextInt();
+            System.out.println("b값을 입력하세요");
+            int num2 = sc.nextInt();
+            System.out.println(num1 + " / " + num2 + " = " + num1 / num2);
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("good bye~~~");
+    }
+}
