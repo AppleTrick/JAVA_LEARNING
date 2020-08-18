011050a55d38dffde46320e51ca10fc86f7469e0
AppleTrick <arrans9591@gmail.com>
2020-08-18 21:37:43 +0900

    Math 함수 사용 random을 통한 난수 생성

---
+++ src/normalClass/Practice4/RandomNumberGenerator.java
@@ -0,0 +1,13 @@
+package normalClass.Practice4;
+
+import java.util.Random;
+
+public class RandomNumberGenerator {
+    public static void main(String[] args) {
+        Random rand = new Random();
+
+        for (int i = 0; i < 7; i++) {
+            System.out.println(rand.nextInt(1000));
+        }
+    }
+}
---
+++ src/normalClass/Practice5/SeedSetRandom.java
@@ -0,0 +1,13 @@
+package normalClass.Practice5;
+
+import java.util.Random;
+
+public class SeedSetRandom {
+    public static void main(String[] args) {
+        Random rand = new Random(System.currentTimeMillis());
+
+        for (int i = 0; i < 7; i++) {
+            System.out.println(rand.nextInt(100));
+        }
+    }
+}
